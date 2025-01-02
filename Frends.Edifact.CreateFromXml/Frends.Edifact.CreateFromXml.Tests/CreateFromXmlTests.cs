099b6805de87bd7ffafb175971f369b8e8b0e9e2
MichalFrends1 <michal.nowakowski@frends.com>
2025-01-02 14:11:49 +0100

    Fix whitespace formatting

--- Frends.Edifact.CreateFromXml/Frends.Edifact.CreateFromXml.Tests/CreateFromXmlTests.cs
+++ Frends.Edifact.CreateFromXml/Frends.Edifact.CreateFromXml.Tests/CreateFromXmlTests.cs
@@ -45,7 +45,7 @@ class CreateFromXmlTests
 
     [Test]
     public void UnsupportedEdifactVersion()
-        {
+    {
         string testData = @"<Edifact>
         <UNH>
             <MessageIdentifier_02>
